d5c28f94c624cb0bd6435a5c30a0baa82a8fb518
David Mills <millsdg-gmail.com>
2024-10-29 12:11:46 +0000

    checks symmetry of 1D and 2D matrices

---
+++ src/MatrixChecker.java
@@ -0,0 +1,26 @@
+public class MatrixChecker {
+    boolean isSymmetrical(int[] myArray){
+        for(int i = 0; i < myArray.length; i++){
+            if(myArray[i] != myArray[myArray.length - (i + 1)]){
+                return false;
+            }
+        }
+        return true;
+    }
+
+    boolean isSymmetrical(int[][] myArray){
+        int outerArrayLen = myArray.length;
+
+        for(int i = 0; i < outerArrayLen; i++){
+            int innerArrayLen = myArray[i].length;
+            for(int j = 0; j < innerArrayLen; j++){
+                if (myArray[i][j] != myArray[outerArrayLen - (i + 1)][innerArrayLen - (j +1)]){
+                    return false;
+            }
+
+            }
+        }
+    return true;
+
+    }
+}
